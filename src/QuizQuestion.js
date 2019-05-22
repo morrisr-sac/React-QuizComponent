85608410b37d3981736b97e6132c39b21237c514
morrisr-sac <50925432+morrisr-sac@users.noreply.github.com>
2019-05-22 12:48:13 -0700

    module2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,22 @@
+import React, { Component} from 'react'
+
+class QuizQuestion extends Component {
+
+  render() {
+    return (
+      <main>
+        <section>
+          <p>// instruction text goes here</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+            <p>{this.props.quiz_question.instruction_text}</p>
+          </ul>
+        </section>
+      </main>
+    )
+  }
+}
+
+export default QuizQuestion
