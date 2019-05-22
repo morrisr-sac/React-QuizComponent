d2e4d31250882a8f631b601da2cd56616efe5a1f
morrisr-sac <50925432+morrisr-sac@users.noreply.github.com>
2019-05-22 12:54:57 -0700

    quizquestionbutton

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class QuizQuestionButton extends Component {
+  render() {
+      return (
+        <li><button>{this.props.buttonText}</button></li>
+      )
+  }
+}
+
+export default QuizQuestionButton
