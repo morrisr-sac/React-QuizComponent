06fe0316270e96aa6ceefa9fe2263fa3df3c1888
morrisr-sac <50925432+morrisr-sac@users.noreply.github.com>
2019-05-22 12:49:50 -0700

    instruction text fix

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,12 +6,12 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p>// instruction text goes here</p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
             <li>{this.props.quiz_question.answer_options[0]}</li>
-            <p>{this.props.quiz_question.instruction_text}</p>
+
           </ul>
         </section>
       </main>
